a18dc5a436d6e1492bcf6396f71cc859a60c52d3
u400r <uk.hitoshi@gmail.com>
2023-07-17 06:02:25 +0000

    implement code generation base class
    - abstructed structure definition represent binary data structure
      definition, instance of classes defined in definition.py
    - definitions can generate python ast tree using TypeBase classes
      as super class
    - generated class can
      - load binary data and instantiate data class using specified definition
      - dump data structure as python dict format
      - and further enhance:
        - update part of binary
        - dump definiton as yaml
        - update part of definition
        - generate python class file using specified definition

---
+++ bin-opts/structure/definition.py
@@ -0,0 +1,219 @@
+
+
+from __future__ import annotations
+from ast import AnnAssign, Assign, Attribute, Call, ClassDef, Constant, FunctionDef, List, Load, Name, Param, Pass, Store, Subscript, Tuple, arg, arguments
+
+from structure.type import TypeBase, PrimitiveType, ArrayType, StructureType
+from structure.type import Uint1, Uint2, Uint4, Uint8, Uint16, Uint32, Uint64
+
+
+import yaml
+
+
+class Definition():
+    """Represent data structure definition
+
+    TODO: It is needed to change to hold structure classes inside of class variable,
+    and each StructureDefinition instance only has a reference to the list.
+    each definition should be a singlton instance.
+    """
+    name: str
+    type: TypeBase
+
+    def __init__(self, **kwargs):
+        if "type" not in kwargs:
+            raise TypeError("type key does not exists")
+        if "name" in kwargs:
+            self.name = kwargs["name"]
+        else:
+            self.name = "aaa"
+
+    def to_dict(self):
+        print(self.name)
+        return {
+            "name": self.name
+        }
+
+    @classmethod
+    def load(cls, **kwargs):
+        if "type" not in kwargs:
+            raise TypeError("type key does not exists")
+
+        if kwargs["type"] == "object":
+            return StructureDefinition(**kwargs)
+        elif kwargs["type"] == "array":
+            return ArrayDefinition(**kwargs)
+        else:
+            return PrimitiveDefinition(**kwargs)
+
+    def dump(self):
+        print(yaml.dump(self.to_dict()))
+
+    def make_node(self):
+        pass
+
+
+class PrimitiveDefinition(Definition):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if issubclass(globals()[kwargs["type"]], PrimitiveType):
+            self.type = globals()[kwargs["type"]]
+        else:
+            raise TypeError("invalid primitive type")
+
+    def to_dict(self):
+        return dict(super().to_dict(), **{
+            "type": self.type.__name__
+        })
+
+    def instantiate(self):
+        return Call(
+                func=Name(id=self.type.__name__, ctx=Load()),
+                args=[Constant(value=self.name)], keywords=[]
+            )
+
+
+class ArrayDefinition(Definition):
+    items: StructureDefinition | ArrayDefinition | PrimitiveDefinition
+    size: int
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if kwargs["type"] != 'array':
+            raise TypeError("invalid array type")
+        else:
+            self.type = ArrayType
+
+        if "items" not in kwargs:
+            raise TypeError("items key does not exists")
+
+        if "size" not in kwargs:
+            raise TypeError("size key does not exists")
+
+        self.items = Definition.load(**kwargs["items"])
+        self.size = kwargs["size"]
+
+    def to_dict(self):
+        _dict = dict(super().to_dict(), **{
+            "type": "array",
+            "size": self.size
+        })
+        _dict["items"] = self.items.to_dict()
+        return _dict
+
+    def make_node(self):
+        return self.items.make_node()
+
+    def instantiate(self):
+        return Call(
+                func=Name(id="ArrayType", ctx=Load()),
+                args=[
+                    Attribute(
+                        value=Name(id='self', ctx=Load()),
+                        attr=self.items.alias, ctx=Load()
+                    )
+                    if isinstance(self.items, StructureDefinition)
+                    else Name(id=self.items.type.__name__, ctx=Load()),
+                    Constant(value=self.size), Constant(value=self.name)], keywords=[]
+        )
+
+
+class StructureDefinition(Definition):
+    alias: str
+    parameters: list[StructureDefinition | ArrayDefinition | PrimitiveDefinition] = []
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if kwargs['type'] != 'object':
+            raise TypeError('invalid object type')
+
+        if "alias" not in kwargs:
+            raise TypeError("alias key does not exists")
+
+        if "parameters" not in kwargs:
+            raise TypeError("parameters key does not exists")
+
+        if not isinstance(kwargs['parameters'], list):
+            raise TypeError("parameters is not list type")
+
+        self.alias = kwargs["alias"]
+        # it is impossible to be forward defining the custom structure class
+        # so assing base class of structure.
+        self.type = StructureType
+        self.parameters = []
+        for p in kwargs["parameters"]:
+            self.parameters.append(Definition.load(**p))
+
+    def to_dict(self):
+        _dict = dict(super().to_dict(), **{
+            "type": "object",
+            "alias": self.alias,
+            "parameters": []
+        })
+        for parameter in self.parameters:
+            _dict["parameters"].append(parameter.to_dict())
+        return _dict
+
+    def make_constructor(self):
+        return FunctionDef(
+            name='__init__',
+            args=arguments(
+                posonlyargs=[],
+                args=[arg(arg='self'), arg(arg='name')],
+                kwonlyargs=[],
+                kw_defaults=[],
+                defaults=[Constant(value=None)]),
+            body=[
+                Assign(
+                    targets=[
+                        Attribute(
+                            value=Name(id='self', ctx=Load()),
+                            attr=p.name,
+                            ctx=Store())
+                        ],
+                    value=p.instantiate()
+                ) for p in self.parameters] + [
+                Assign(
+                    targets=[
+                        Attribute(value=Name(id='self', ctx=Load()),
+                                  attr='_parameters', ctx=Store())
+                    ],
+                    value=List(
+                        elts=[Attribute(
+                            value=Name(id='self', ctx=Load()),
+                            attr=p.name,
+                            ctx=Load()) for p in self.parameters],
+                        ctx=Load()
+                    )
+                ),
+                Assign(
+                    targets=[
+                        Attribute(value=Name(id='self', ctx=Load()),
+                                  attr='__name__', ctx=Store())
+                    ],
+                    value=Name(id='name', ctx=Load()),
+                    ctx=Load()
+                )
+            ],
+            decorator_list=[])
+
+    def make_node(self):
+        return ClassDef(
+            name=self.alias,
+            body=[p.make_node() for p in self.parameters if p.make_node()] + [
+                self.make_constructor()],
+            decorator_list=[],
+            bases=[
+                Name(id='StructureType', ctx=Load())
+                   ],
+            keywords=[],
+        )
+
+    def instantiate(self):
+        return Call(
+                func=Attribute(
+                    value=Name(id='self', ctx=Load()),
+                    attr=self.alias, ctx=Load()),
+                args=[Constant(value=self.name)], keywords=[]
+            )
---
+++ bin-opts/structure/type.py
@@ -0,0 +1,172 @@
+from __future__ import annotations
+from abc import ABC
+from typing import Callable, Sized
+
+
+class TypeBase(ABC):
+
+    def __init__(self, name=None):
+        self.__name__ = name
+
+    def raw(self) -> bytearray:
+        pass
+
+    def load(self, data: bytes, pos: int = 0):
+        pass
+
+    def dump(self) -> bytearray:
+        pass
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class PrimitiveType(TypeBase):
+    _size: int
+    _format: Callable[[PrimitiveType, bytearray], str]
+    _start: int
+    _end: int
+
+    def __init__(self, name=None):
+        self.__name__ = name
+        self._data = bytearray((0).to_bytes(self._size))
+
+    def to_dict(self):
+        return {self.__name__: self._format(self._data)} \
+            if self.__name__ else self._format(self._data)
+
+    def raw(self) -> bytearray:
+        return self._data
+
+    def load(self,  data: bytes, pos: int = 0):
+        self._data[:] = data[pos:pos+self._size]
+
+    def dump(self) -> bytearray:
+        return self._data
+
+    def __len__(self):
+        return self._size
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+def singed_fromatter(t: PrimitiveType, data: bytearray):
+    return data.hex()
+
+
+def unsinged_fromatter(t: PrimitiveType, data: bytearray):
+    return data.hex()
+
+
+# builtin type definition
+class Uint1(PrimitiveType):
+    _size = 1
+    _format = unsinged_fromatter
+
+
+class Uint2(PrimitiveType):
+    _size = 2
+    _format = unsinged_fromatter
+
+
+class Uint4(PrimitiveType):
+    _size = 4
+    _format = unsinged_fromatter
+
+
+class Uint8(PrimitiveType):
+    _size = 8
+    _format = unsinged_fromatter
+
+
+class Uint16(PrimitiveType):
+    _size = 16
+    _format = unsinged_fromatter
+
+
+class Uint32(PrimitiveType):
+    _size = 32
+    _format = unsinged_fromatter
+
+
+class Uint64(PrimitiveType):
+    _size = 64
+    _format = unsinged_fromatter
+
+
+class ArrayType(TypeBase, Sized):
+
+    def __init__(self, item, size, name=None):
+        self.__name__ = name
+        self._size = size
+        # todo: must consider recursive tuple definition
+        self._items = tuple(item() for i in range(self._size))
+
+    def to_dict(self):
+        children = []
+
+        for item in self._items:
+            children.append(item.to_dict())
+        if self.__name__:
+            return {self.__name__: children}
+        else:
+            return children
+
+    def load(self, data: bytes, pos: int = 0):
+        for item in self._items:
+            item.load(data, pos)
+            pos += len(item)
+
+    def dump(self) -> bytearray:
+        return b"".join([item.dump() for item in self._items])
+
+    def raw(self):
+        pass
+
+    def __len__(self):
+        return sum([len(item) for item in self._items])
+
+    def __str__(self):
+        return ""
+
+
+class StructureType(TypeBase, Sized):
+    _parameters: list[StructureType | ArrayType | PrimitiveType] = []
+
+    def __init__(self, name=None):
+        self.__name__ = name
+
+    def load(self, data: bytes, pos: int = 0):
+        for parameter in self._parameters:
+            parameter.load(data, pos)
+            pos += len(parameter)
+
+    def dump(self):
+        return b"".join([p.dump() for p in self._parameters])
+
+    def raw(self):
+        return b""
+
+    def to_dict(self):
+        children = {}
+        for parameter in self._parameters:
+            children.update(parameter.to_dict())
+        if self.__name__:
+            _dict = {self.__name__: children}
+        else:
+            _dict = children
+        return _dict
+
+    def __len__(self):
+        return sum([len(p) for p in self._parameters])
+
+    def __str__(self):
+        return ""
+    # binary -> instance: load
+    # instance -> binary: dump
+    # class -> yaml: definition
+    # yaml -> class: ast.compile?
